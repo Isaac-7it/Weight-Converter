32f086395453598d68dbb39bb428e0069563db74
dev AIO <isaacmail.07@gmail.com>
2025-08-29 15:14:01 +0100

    commiting first Python file 😀

---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+unit = input('(L)bs or (K)g: ')
+weight = int(input('Weight: '))
+
+if unit.lower() == 'l':
+    weight *= 0.45
+    print(f'You are {weight} kilos')
+elif unit.lower() == 'k':
+    weight *= 2.21
+    print(f'You are {weight} pounds')
+# Yayyy! I officially pushed my First Python project to Github
